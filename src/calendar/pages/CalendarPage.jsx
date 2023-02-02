9d6697877e60af806860b68a90fbde6c166780fd
Manuel Ulate <fr3zs09@gmail.com>
2023-02-02 13:57:56 -0600

    componente de calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,46 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { format, parse, startOfWeek, getDay, addHours } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
 import { Navbar } from '../';
+
+const locales = {
+  'en-US': enUS
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Cumple',
+  notes: 'Comprar qq',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: '#fafafa',
+  user: {
+    _id: 'ABC',
+    name: 'Manuel'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+      <div className='container shadow-sm p-4'>
+        <Calendar
+          localizer={ localizer }
+          events={ events }
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 'calc( 90vh - 100px )' }}
+        />
+      </div>
     </>
   )
 }
